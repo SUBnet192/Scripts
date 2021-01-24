c24e2e7b7753d207e3ef6aa1bd7baee71596e56b
Marc Bouchard <marc@subnet192.com>
2021-01-23 19:47:42 -0500

    Update Build-OfflineRootCA.ps1

--- Build-OfflineRootCA.ps1
+++ Build-OfflineRootCA.ps1
@@ -5,36 +5,37 @@ Clear-Host
 Write-Host "Building Offline Root CA" -ForegroundColor Green
 write-host "`n"
 
-Write-Host "... Setting default shell to Powershell"
+Write-Host "... Setting default shell to Powershell" -ForegroundColor Green
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion\WinLogon' -Name Shell -Value 'PowerShell.exe' | Out-null
 
-Write-Host "... Creating C:\Scripts folder"
+Write-Host "... Creating C:\Scripts folder" -ForegroundColor Green
 New-Item -Path C:\ -Name Scripts -ItemType Directory -Force | Out-Null
 
-Write-Host "... Retrieving CAPolicy.inf from Github"
+Write-Host "... Retrieving CAPolicy.inf from Github" -ForegroundColor Green
 Invoke-WebRequest -usebasicparsing -Uri "https://raw.githubusercontent.com/SUBnet192/inf/main/CAPolicy.inf.offlineroot" -Outfile "C:\Windows\CAPolicy.inf"
 
-Write-Host "... Editing CAPolicy.inf"
-Start-Process -Wait -FilePath "notepad.exe" -ArgumentList "c:\windows\capolicy.inf"
-write-host "`n"
-Get-Content C:\Windows\CAPolicy.inf
-write-host "`n"
-
 do {
-    Write-Host 'Are you satisfied with the contents of CAPolicy.inf? (y/n)' -NoNewline
+    Write-Host "... Editing CAPolicy.inf" -ForegroundColor Green
+    Start-Process -Wait -FilePath "notepad.exe" -ArgumentList "c:\windows\capolicy.inf"
+    write-host "`n"
+    Get-Content C:\Windows\CAPolicy.inf
+    write-host "`n"
+    Write-Host 'Are you satisfied with the contents of CAPolicy.inf? (y/n)' -NoNewline -ForegroundColor Yellow
     $response = Read-Host
-
-    if ($response -eq 'n') {
-       Start-Process -Wait -FilePath "notepad.exe" -ArgumentList "c:\windows\capolicy.inf"
-       write-host "`n"
-       Get-Content 'C:\Windows\CAPolicy.inf'
-       write-host "`n"
-    }
 } until ($response -eq 'y')
 
-# Install AD Certificate Services
+$response = $null
+
+Write-Host "... Install AD Certificate Services" -ForegroundColor Green
 Add-WindowsFeature -Name ADCS-Cert-Authority -IncludeManagementTools
 
-# Configure AD Certificate Services
-$OfflineCAName = 'Corp-Root-CA'
+Write-Host "... Configure AD Certificate Services" -ForegroundColor Green
+do {
+    Write-Host 'Enter the Common Name for the Offline root CA (ex: Corp-Root-CA):' -NoNewline -ForegroundColor Yellow
+    $OfflineCAName = Read-Host
+    Write-Host "Are you satisfied with the CA Name '$OfflineCAName'?" -NoNewline -ForegroundColor Yellow
+    $response = Read-Host
+} until ($response -eq 'y')
+
+$response = $null
 Install-AdcsCertificationAuthority -CAType EnterpriseRootCa -CryptoProviderName 'RSA#Microsoft Software Key Storage Provider' -KeyLength 4096 -HashAlgorithmName SHA256 -ValidityPeriod Years -ValidityPeriodUnits 5 -CACommonName $OfflineCAName
