cd08edb604fd07671bca98a8e248859369ccbbb0
Marc Bouchard <marc@subnet192.com>
2021-01-26 08:43:13 -0500

    Update Setup-CoreBasics.ps1

--- Setup-CoreBasics.ps1
+++ Setup-CoreBasics.ps1
@@ -1,1 +1,5 @@
+# Call this script from a powershell command prompt using this command:
+# Invoke-WebRequest -usebasicparsing -uri "https://raw.githubusercontent.com/SUBnet192/Scripts/master/Setup-CoreBasics.ps1" | Invoke-Expression
+
+# Set Powershell as default shell
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\' -Name Shell -Value 'powershell.exe'
