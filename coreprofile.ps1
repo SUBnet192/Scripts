e3c1830cffa371f455f25f11ae57d24f4c4ebd6d
Marc Bouchard <marc@subnet192.com>
2021-01-27 08:05:24 -0500

    Update coreprofile.ps1

--- coreprofile.ps1
+++ coreprofile.ps1
@@ -36,12 +36,9 @@ Set-PSReadlineOption -ShowToolTips -BellStyle Visual
 # General Helper Functions
 #======================================================================================
 
-function reboot { shutdown /r /t 5 }
-function halt { shutdown /s /t 5 }
-function here { Invoke-Item . }
-function Find-Files ([string] $glob) { get-childitem -recurse -include $glob }
-function Remove-Directory ([string] $glob) { remove-item -recurse -force $glob }
-function Edit-Profile() { vsc $PROFILE.AllUsersAllHosts }
+function reboot { shutdown /r /t 0 }
+function halt { shutdown /s /t 0 }
+function Edit-Profile() { notepad $PROFILE.AllUsersAllHosts }
 function Clear-Logs { wevtutil el | % {wevtutil cl $_} }
 
 #======================================================================================
@@ -60,6 +57,89 @@ function Start-PsElevatedSession {
   }
   Else { Write-Warning "Session is already elevated" }
 } 
+
+#======================================================================================
+
+function Get-MOTD {
+    # Perform WMI Queries
+    $Wmi_OperatingSystem = Get-WmiObject -Query 'Select lastbootuptime,TotalVisibleMemorySize,FreePhysicalMemory,caption,version From win32_operatingsystem'
+    $Wmi_Processor = Get-WmiObject -Query 'Select Name,LoadPercentage From Win32_Processor'
+    $Wmi_LogicalDisk = Get-WmiObject -Query 'Select Size,FreeSpace From Win32_LogicalDisk Where DeviceID="C:"'
+
+    # Assign variables
+    $Date = Get-Date
+    $OS = $Wmi_Operatingsystem.Caption
+    $Kernel = $Wmi_Operatingsystem.Version
+    $Uptime = "$(($Uptime = $Date - $Wmi_Operatingsystem.ConvertToDateTime($Wmi_Operatingsystem.LastBootUpTime)).Days) days, $($Uptime.Hours) hours, $($Uptime.Minutes) minutes"
+    $Shell = "{0}.{1}" -f $PSVersionTable.PSVersion.Major,$PSVersionTable.PSVersion.Minor
+    $CPU = $Wmi_Processor.Name -replace '\(C\)', '' -replace '\(R\)', '' -replace '\(TM\)', '' -replace 'CPU', '' -replace '\s+', ' '
+    $Processes = (Get-Process).Count
+    $CurrentLoad = $Wmi_Processor.LoadPercentage
+    $Memory = "{0}mb/{1}mb Used" -f (([math]::round($Wmi_Operatingsystem.TotalVisibleMemorySize/1KB))-([math]::round($Wmi_Operatingsystem.FreePhysicalMemory/1KB))),([math]::round($Wmi_Operatingsystem.TotalVisibleMemorySize/1KB))
+    $Disk = "{0}gb/{1}gb Used" -f (([math]::round($Wmi_LogicalDisk.Size/1GB))-([math]::round($Wmi_LogicalDisk.FreeSpace/1GB))),([math]::round($Wmi_LogicalDisk.Size/1GB))
+
+    Write-Host ("")
+    Write-Host ("")
+    Write-Host ("         ,.=:^!^!t3Z3z.,                  ") -ForegroundColor Red
+    Write-Host ("        :tt:::tt333EE3                    ") -ForegroundColor Red
+    Write-Host ("        Et:::ztt33EEE ") -NoNewline -ForegroundColor Red
+    Write-Host (" @Ee.,      ..,     ") -NoNewline -ForegroundColor Green
+    Write-Host $Date -ForegroundColor Green
+    Write-Host ("       ;tt:::tt333EE7") -NoNewline -ForegroundColor Red
+    Write-Host (" ;EEEEEEttttt33#     ") -ForegroundColor Green
+    Write-Host ("      :Et:::zt333EEQ.") -NoNewline -ForegroundColor Red
+    Write-Host (" SEEEEEttttt33QL     ") -NoNewline -ForegroundColor Green
+    Write-Host ("User: ") -NoNewline -ForegroundColor Red
+    Write-Host ("$env:username") -ForegroundColor Cyan
+    Write-Host ("      it::::tt333EEF") -NoNewline -ForegroundColor Red
+    Write-Host (" @EEEEEEttttt33F      ") -NoNewline -ForeGroundColor Green
+    Write-Host ("Hostname: ") -NoNewline -ForegroundColor Red
+    Write-Host ("$env:computername") -ForegroundColor Cyan
+    Write-Host ("     ;3=*^``````'*4EEV") -NoNewline -ForegroundColor Red
+    Write-Host (" :EEEEEEttttt33@.      ") -NoNewline -ForegroundColor Green
+    Write-Host ("OS: ") -NoNewline -ForegroundColor Red
+    Write-Host $OS -ForegroundColor Cyan
+    Write-Host ("     ,.=::::it=., ") -NoNewline -ForegroundColor Cyan
+    Write-Host ("``") -NoNewline -ForegroundColor Red
+    Write-Host (" @EEEEEEtttz33QF       ") -NoNewline -ForegroundColor Green
+    Write-Host ("Kernel: ") -NoNewline -ForegroundColor Red
+    Write-Host ("NT ") -NoNewline -ForegroundColor Cyan
+    Write-Host $Kernel -ForegroundColor Cyan
+    Write-Host ("    ;::::::::zt33) ") -NoNewline -ForegroundColor Cyan
+    Write-Host ("  '4EEEtttji3P*        ") -NoNewline -ForegroundColor Green
+    Write-Host ("Uptime: ") -NoNewline -ForegroundColor Red
+    Write-Host $Uptime -ForegroundColor Cyan
+    Write-Host ("   :t::::::::tt33.") -NoNewline -ForegroundColor Cyan
+    Write-Host (":Z3z.. ") -NoNewline -ForegroundColor Yellow
+    Write-Host (" ````") -NoNewline -ForegroundColor Green
+    Write-Host (" ,..g.        ") -NoNewline -ForegroundColor Yellow
+    Write-Host ("Shell: ") -NoNewline -ForegroundColor Red
+    Write-Host ("Powershell $Shell") -ForegroundColor Cyan
+    Write-Host ("   i::::::::zt33F") -NoNewline -ForegroundColor Cyan
+    Write-Host (" AEEEtttt::::ztF         ") -NoNewline -ForegroundColor Yellow
+    Write-Host ("CPU: ") -NoNewline -ForegroundColor Red
+    Write-Host $CPU -ForegroundColor Cyan
+    Write-Host ("  ;:::::::::t33V") -NoNewline -ForegroundColor Cyan
+    Write-Host (" ;EEEttttt::::t3          ") -NoNewline -ForegroundColor Yellow
+    Write-Host ("Processes: ") -NoNewline -ForegroundColor Red
+    Write-Host $Processes -ForegroundColor Cyan
+    Write-Host ("  E::::::::zt33L") -NoNewline -ForegroundColor Cyan
+    Write-Host (" @EEEtttt::::z3F          ") -NoNewline -ForegroundColor Yellow
+    Write-Host ("Current Load: ") -NoNewline -ForegroundColor Red
+    Write-Host $CurrentLoad -NoNewline -ForegroundColor Cyan
+    Write-Host ("%") -ForegroundColor Cyan
+    Write-Host (" {3=*^``````'*4E3)") -NoNewline -ForegroundColor Cyan
+    Write-Host (" ;EEEtttt:::::tZ``          ") -NoNewline -ForegroundColor Yellow
+    Write-Host ("Memory: ") -NoNewline -ForegroundColor Red
+    Write-Host $Memory -ForegroundColor Cyan
+    Write-Host ("             ``") -NoNewline -ForegroundColor Cyan
+    Write-Host (" :EEEEtttt::::z7            ") -NoNewline -ForegroundColor Yellow
+    Write-Host ("Disk: ") -NoNewline -ForegroundColor Red
+    Write-Host $Disk -ForegroundColor Cyan
+    Write-Host ("                 'VEzjt:;;z>*``           ") -ForegroundColor Yellow
+    Write-Host ("                      ````                  ") -ForegroundColor Yellow
+    Write-Host ("")
+}
 
 #======================================================================================
 # 'Go' command and targets
@@ -106,10 +186,7 @@ function Global:Prompt {
 #======================================================================================
 
 Set-Alias -name su -Value Start-PsElevatedSession
-Set-Alias -name npp -Value Notepad++.exe
-Set-Alias -name vsc -value code
 Set-Alias -Name ff -Value Find-Files 
-Set-Alias -Name rmd -value Remove-Directory 
 Set-Alias -name ih -value invoke-history
 
 #======================================================================================
@@ -122,34 +199,6 @@ Go scripts
 # Some Sysadmin sillyness
 #======================================================================================
 
-$block = @"
- 
-.     .       .  .   . .   .   . .    +  .
-  .     .  :     .    .. :. .___---------___.
-       .  .   .    .  :.:. _".^ .^ ^.  '.. :"-_. .                PowerShell $($PSVersionTable.PSVersion.ToString())
-    .  :       .  .  .:../:            . .^  :.:\.
-        .   . :: +. :.:/: .   .    .        . . .:\               Logged in user: $(whoami)
- .  :    .     . _ :::/:               .  ^ .  . .:\              Elevated Privileges: $(Test-Administrator)
-  .. . .   . - : :.:./.                        .  .:\
-  .      .     . :..|:                    .  .  ^. .:|
-    .       . : : ..||        .                . . !:|
-  .     . . . ::. ::\(                           . :)/
- .   .     : . : .:.|. ######              .#######::|
-  :.. .  :-  : .:  ::|.#######           ..########:|
- .  .  .  ..  .  .. :\ ########          :######## :/
-  .        .+ :: : -.:\ ########       . ########.:/
-    .  .+   . . . . :.:\. #######       #######..:/
-      :: . . . . ::.:..:.\           .   .   ..:/
-   .   .   .  .. :  -::::.\.       | |     . .:/
-      .  :  .  .  .-:.":.::.\             ..:/
- .      -.   . . . .: .:::.:.\.           .:/
-.   .   .  :      : ....::_:..:\   ___.  :/
-   .   .  .   .:. .. .  .: :.:.:\       :/
-     +   .   .   : . ::. :.:. .:.|\  .:/|
-     .         +   .  .  ...:: ..|  --.:|
-.      . . .   .  .  . ... :..:.."(  ..)"
- .   .       .      :  .   .: ::/  .  .::\
- 
-"@
- 
+Get-MOTD
+
 Write-Host $block -ForegroundColor Green
