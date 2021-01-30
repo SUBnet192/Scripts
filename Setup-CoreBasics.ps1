1efc1e329bb17a90d299ac452140520b87966837
Marc Bouchard <marc@subnet192.com>
2021-01-30 11:39:55 -0500

    Update Setup-CoreBasics.ps1

--- Setup-CoreBasics.ps1
+++ Setup-CoreBasics.ps1
@@ -7,6 +7,9 @@ Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlo
 # Trust PowerShell Gallery
 Set-PSRepository PSGallery -InstallationPolicy Trusted
 
+# Install Default Modules
+Install-Module -Name PSWindowsUpdate
+
 # Set Execution Policy
 Set-ExecutionPolicy RemoteSigned -Force
 
@@ -22,6 +25,9 @@ Invoke-WebRequest -usebasicparsing -uri "https://raw.githubusercontent.com/haava
 
 # Create default powershell profile for All Users / All Hosts
 Invoke-WebRequest -usebasicparsing -Uri "https://raw.githubusercontent.com/SUBnet192/Scripts/master/coreprofile.ps1" -Outfile $PROFILE.AllusersAllHosts
+
+# Execute Windows Update
+Install-WindowsUpdate -Confirm: $False
 
 # Reboot to complete installation
 Restart-Computer
