eaae09377606d01d44c57c8fac0ea962ab2b9149
Marc Bouchard <marc@subnet192.com>
2021-05-30 11:39:30 -0400

    Replaced Choco with Winget

--- Build-AdminJumpPoint.ps1
+++ Build-AdminJumpPoint.ps1
@@ -35,42 +35,29 @@ Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCEIP $false -InvalidCert
 Install-Module -Name Testimo
 Install-Module -Name DSInternals
 Install-Module -Name PSPKI
+Install-Module -Name dbatools
 Find-Module -Name SUBNET192* | Install-Module
 
-# Chocolatey tools
-Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -uri "https://chocolatey.org/install.ps1" -UseBasicParsing | Invoke-Expression
-Choco install chocolateygui -y
-
-# Essential tools
-Choco install notepadplusplus -y
-Choco install googlechrome -y
-Choco install adobereader -y
-Choco install 7zip -y
-Choco install winscp -y
-Choco install filezilla -y
-Choco install openssh -y
-Choco install git -y
-Choco install ad-tidy-free -y
-
-# Microsoft Tools
-Choco install sysinternals -y
-Choco install vscode -y
-Choco install vscode-powershell -y
-Choco install azcopy -y
-
-# SQL Related
-Choco install sql-server-management-studio -y
-Choco install dbatools -y
-
-# Vmware related
-Choco install rvtools -y
+# Winget tools
+$dlPath = 'C:\Sources\WinGet.appxbundle'
+Invoke-WebRequest 'https://aka.ms/getwinget' -OutFile $dlPath
+Add-AppxPackage $dlPath
+winget install -h -e --id 7zip.7zip
+winget install -h -e --id Adobe.AdobeAcrobatReaderDC
+winget install -h -e --id Git.Git
+winget install -h -e --id Google.Chrome
+winget install -h -e --id Microsoft.AzureCLI
+winget install -h -e --id Microsoft.SQLServerManagementStudio
+winget install -h -e --id Microsoft.VisualStudioCode
+winget install -h -e --id Notepad++.Notepad++
+winget install -h -e --id TimKosse.FileZillaClient
+winget install -h -e --id WinSCP.WinSCP
 
 # SpecOps GPUpdate
 Invoke-WebRequest -Uri "https://download.specopssoft.com/Release/gpupdate/specopsgpupdatesetup.exe" -OutFile C:\Sources\specops.exe
 7z x C:\Sources\specops.exe -oC:\Temp\
-Start-Process -FilePath "$env:systemroot\system32\msiexec.exe" -ArgumentList '/i "C:\Temp\Products\SpecOpsGPUpdate\SpecopsGpupdate-x64.msi" /qb' -Wait
+Start-Process -FilePath "$env:systemroot\system32\msiexec.exe" -ArgumentList '/i "C:\Temp\Products\SpecOpsGPUpdate\SpecopsGpupdate-x64.msi"' -Wait
 Remove-Item -Path C:\Temp -Recurse -Force
-Remove-Item C:\Scripts\specops.exe
 
 # Create default powershell profile for All Users / All Hosts
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/SUBnet192/Scripts/master/psprofile.ps1" -Outfile $PROFILE.AllusersAllHosts
